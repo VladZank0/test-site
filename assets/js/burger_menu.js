bd171b333beac3cef2606fedaf9951ccaf5e1023
Vlad Zanko <vlad.zanko.2019@mail.ru>
2023-09-15 12:06:21 +0300

    first-commit

---
+++ assets/js/burger_menu.js
@@ -0,0 +1,8 @@
+(function(){
+    let btn = document.querySelector('.burger-btn');
+    let menu = document.querySelector('.burger-menu');
+    btn.addEventListener('click', (e) => {
+        menu.classList.toggle('active');пше
+    })
+
+}())
